52042152cd67a79be7d1052eb1c28a428af47cbc
Colin Whitney <colin.whitney@gmail.com>
2020-10-09 16:09:28 -0400

    skeleton app set up; api linked; populating questions from opentdb working

--- millionaire/app.js
+++ millionaire/app.js
@@ -1,5 +1,14 @@
-if (typeof $ === 'undefined') {
-  console.log('you blew it')
-} else {
-  console.log('you did it')
-}
+
+$(() => {
+  $.ajax({
+    url: "https://opentdb.com/api.php?amount=1&type=multiple",
+  }).then((data) => {
+    console.log(data)
+    $('<h3>').text(data.results[0].question).insertAfter('h1')
+    $('<div>').text(data.results[0].correct_answer).addClass('question').attr('id', 'correct').appendTo('.container')
+    for (let i = 0; i < data.results[0].incorrect_answers.length; i++){
+      $('<div>').text(data.results[0].incorrect_answers[i]).addClass('question').attr('id', 'wrong').appendTo('.container')
+    }
+    
+  })
+})
